7ff6b73a10590a15cf01e77a3c6c5546ab39fc33
starsys <theophile.guettier@gmail.com>
2018-10-05 13:55:58 +0200

    NewGrid class creation. Maze.txt update

--- p3.py
+++ p3.py
@@ -1,7 +1,14 @@
 # -*- coding: utf8 -*-
 
 
+class NewGrid:
+
+    def __init__(self, datafile):
+        self.datafile = datafile
+
+
+
 class Character:
 
-    def __init__(self):
-        pass
+    def __init__(self, good):
+        self.good = good
